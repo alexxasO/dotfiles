f3442930166d055cec2a4268f95e97878b328703
alexxasO <alexis.jouquan@epitech.eu>
2025-06-26 14:26:50 +0200

    feat : added ubuntu install script

---
+++ install_ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt install -y \
+    acpi arandr arc-theme fonts-powerline dex dmenu dunst feh galculator \
+    gvfs gvfs-backends i3 i3blocks i3lock i3status jq lightdm \
+    lightdm-gtk-greeter mpv network-manager-gnome numlockx playerctl rofi \
+    scrot sysstat thunar thunar-archive-plugin thunar-volman tumbler unzip \
+    xarchiver xbindkeys xdg-user-dirs-gtk pluma xfce4-terminal xbacklight \
+    x11-utils xss-lock zip papirus-icon-theme policykit-1-gnome
